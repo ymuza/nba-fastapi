cdcdc04365734ffbfe01cc51c61e3f84b842fb5a
yamil <yamil.muza@gmail.com>
2024-03-29 16:12:04 -0300

    sdsdsd

--- database.py
+++ database.py
@@ -4,7 +4,6 @@ from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DTABASE_URL = "postgresql://yamil:32874993@localhost:5432/nba"
 
-
 engine = create_engine(SQLALCHEMY_DTABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
